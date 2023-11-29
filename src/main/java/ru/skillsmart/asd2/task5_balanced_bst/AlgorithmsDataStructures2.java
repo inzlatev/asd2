6d13a9e2e6cec39a4c8112cd7f0ad1b426572be3
Ivan Zlatev <ivan.zlatev@t-systems.com>
2023-11-30 00:44:36 +0100

    task5 - balanced BST

--- src/main/java/ru/skillsmart/asd2/task5_balanced_bst/AlgorithmsDataStructures2.java
+++ src/main/java/ru/skillsmart/asd2/task5_balanced_bst/AlgorithmsDataStructures2.java
@@ -12,23 +12,19 @@ public class AlgorithmsDataStructures2 {
         }
         Arrays.sort(a);
         int[] resultingArray = new int[a.length];
-        FillArray(a, 0, resultingArray);
+        FillArray(a, 0, 0, a.length - 1, resultingArray);
 
         return resultingArray;
     }
 
-    private static void FillArray(int[] currentArray, int resultingArrayIndex, int[] resultingArray) {
-        int currentArrayIndex = currentArray.length / 2;
-        resultingArray[resultingArrayIndex] = currentArray[currentArrayIndex];
-        if (currentArrayIndex == 0)
+    private static void FillArray(int[] initialArray, int resultingArrayIndex, int start, int end, int[] resultingArray) {
+        int middle = (start + end) / 2;
+        resultingArray[resultingArrayIndex] = initialArray[middle];
+        if (middle == start && middle == end)
             return;
 
-        int[] leftArrayPart = Arrays.stream(currentArray)
-                .filter(a -> a < currentArray[currentArrayIndex]).toArray();
-        int[] rightArrayPart = Arrays.stream(currentArray)
-                .filter(a -> a > currentArray[currentArrayIndex]).toArray();
-        FillArray(leftArrayPart, resultingArrayIndex * 2 + 1, resultingArray);
-        FillArray(rightArrayPart, resultingArrayIndex * 2 + 2, resultingArray);
+        FillArray(initialArray, resultingArrayIndex * 2 + 1, start, middle - 1, resultingArray);
+        FillArray(initialArray, resultingArrayIndex * 2 + 2, middle + 1, end, resultingArray);
     }
 
     private static boolean CheckArraySize(int arraySize) {
